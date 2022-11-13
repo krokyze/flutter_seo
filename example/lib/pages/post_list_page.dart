b838c1e6b99b756fd5ce7df1ffe47828e29b065e
Martin Anderson <martins.andersons@hotmail.com>
2022-11-14 00:40:14 +0100

    create example widgets (#7)
    * create example widgets
    
    * no message

--- example/lib/pages/post_list_page.dart
+++ example/lib/pages/post_list_page.dart
@@ -1,8 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:seo/seo.dart';
 import 'package:seo_example/main_router.dart';
 import 'package:seo_example/post.dart';
+import 'package:seo_example/widgets/app_image.dart';
+import 'package:seo_example/widgets/app_link.dart';
+import 'package:seo_example/widgets/app_text.dart';
 
 class PostListPage extends StatelessWidget {
   const PostListPage({super.key});
@@ -28,12 +30,11 @@ class _Card extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const baseHref = '/flutter_seo'; // needed because of --base-href
     final route = PostDetailsRoute(id: post.id);
 
-    return Seo.link(
+    return AppLink(
       anchor: post.title,
-      href: baseHref + route.fullPath,
+      href: route.fullPath,
       child: MouseRegion(
         cursor: SystemMouseCursors.click,
         child: GestureDetector(
@@ -44,14 +45,11 @@ class _Card extends StatelessWidget {
             padding: const EdgeInsets.all(16.0),
             child: Row(
               children: [
-                Seo.image(
+                AppImage(
                   alt: post.title,
                   src: post.imageSmall,
-                  child: Image.network(
-                    post.imageSmall,
-                    width: 64,
-                    height: 64,
-                  ),
+                  width: 64,
+                  height: 64,
                 ),
                 Expanded(
                   child: Padding(
@@ -59,32 +57,23 @@ class _Card extends StatelessWidget {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Seo.text(
+                        AppText(
                           text: post.title,
-                          child: Text(
-                            post.title,
-                            style: Theme.of(context).textTheme.headline6,
-                          ),
+                          style: Theme.of(context).textTheme.headline6,
                         ),
                         Padding(
                           padding: const EdgeInsets.only(top: 4.0),
                           child: Row(
                             children: [
-                              Seo.text(
+                              AppText(
                                 text: post.author,
-                                child: Text(
-                                  post.author,
-                                  style: Theme.of(context).textTheme.caption,
-                                ),
+                                style: Theme.of(context).textTheme.caption,
                               ),
                               Padding(
                                 padding: const EdgeInsets.only(left: 8.0),
-                                child: Seo.text(
+                                child: AppText(
                                   text: post.date,
-                                  child: Text(
-                                    post.date,
-                                    style: Theme.of(context).textTheme.caption,
-                                  ),
+                                  style: Theme.of(context).textTheme.caption,
                                 ),
                               ),
                             ],
---
+++ example/lib/widgets/app_text.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:seo/seo.dart';
+
+class AppText extends StatelessWidget {
+  final String text;
+  final TextStyle? style;
+
+  const AppText({
+    super.key,
+    required this.text,
+    required this.style,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Seo.text(
+      text: text,
+      child: Text(
+        text,
+        style: style,
+      ),
+    );
+  }
+}
