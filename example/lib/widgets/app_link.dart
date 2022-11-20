af14e1869cb680603d2385b4ae92659338586ee3
Martin Anderson <martins.andersons@hotmail.com>
2022-11-20 22:38:23 +0200

    add google-site-verification (#12)

--- example/lib/widgets/app_link.dart
+++ example/lib/widgets/app_link.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 import 'package:seo/seo.dart';
 
 class AppLink extends StatelessWidget {
--- example/lib/widgets/app_text.dart
+++ example/lib/widgets/app_text.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:seo/seo.dart';
 
 class AppText extends StatelessWidget {
