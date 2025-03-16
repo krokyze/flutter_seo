cb0efed9faf29d7ac7ee5624d1b9a684917d9194
Martin Anderson <martins.andersons@hotmail.com>
2025-03-16 22:55:36 +0200

    [CHORE] bump flutter version (#104)

--- example/lib/main_router.gr.dart
+++ example/lib/main_router.gr.dart
@@ -1,3 +1,4 @@
+// dart format width=80
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
 // **************************************************************************
@@ -12,19 +13,13 @@ part of 'main_router.dart';
 /// generated route for
 /// [PostDetailsPage]
 class PostDetailsRoute extends PageRouteInfo<PostDetailsRouteArgs> {
-  PostDetailsRoute({
-    Key? key,
-    required int id,
-    List<PageRouteInfo>? children,
-  }) : super(
-          PostDetailsRoute.name,
-          args: PostDetailsRouteArgs(
-            key: key,
-            id: id,
-          ),
-          rawPathParams: {'id': id},
-          initialChildren: children,
-        );
+  PostDetailsRoute({Key? key, required int id, List<PageRouteInfo>? children})
+    : super(
+        PostDetailsRoute.name,
+        args: PostDetailsRouteArgs(key: key, id: id),
+        rawPathParams: {'id': id},
+        initialChildren: children,
+      );
 
   static const String name = 'PostDetailsRoute';
 
@@ -33,20 +28,15 @@ class PostDetailsRoute extends PageRouteInfo<PostDetailsRouteArgs> {
     builder: (data) {
       final pathParams = data.inheritedPathParams;
       final args = data.argsAs<PostDetailsRouteArgs>(
-          orElse: () => PostDetailsRouteArgs(id: pathParams.getInt('id')));
-      return PostDetailsPage(
-        key: args.key,
-        id: args.id,
+        orElse: () => PostDetailsRouteArgs(id: pathParams.getInt('id')),
       );
+      return PostDetailsPage(key: args.key, id: args.id);
     },
   );
 }
 
 class PostDetailsRouteArgs {
-  const PostDetailsRouteArgs({
-    this.key,
-    required this.id,
-  });
+  const PostDetailsRouteArgs({this.key, required this.id});
 
   final Key? key;
 
@@ -62,10 +52,7 @@ class PostDetailsRouteArgs {
 /// [PostListPage]
 class PostListRoute extends PageRouteInfo<void> {
   const PostListRoute({List<PageRouteInfo>? children})
-      : super(
-          PostListRoute.name,
-          initialChildren: children,
-        );
+    : super(PostListRoute.name, initialChildren: children);
 
   static const String name = 'PostListRoute';
 
